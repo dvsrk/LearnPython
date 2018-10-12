abe4788fde7e5b2de7d49a348bb859cdec01550a
dasaris1 <dasarishiva@gmail.com>
2018-10-11 22:27:50 -0700

    renamed files

---
+++ 02_calculate_sum_of_integers.py
@@ -0,0 +1,15 @@
+
+import cProfile
+
+def sum1(n):
+    final_sum =0
+
+    for x in range(n+1):
+        final_sum += x
+    return final_sum
+
+def sum2(n):
+    return (n*(n+1)/2)
+
+print sum1(10)
+print sum2(10)
