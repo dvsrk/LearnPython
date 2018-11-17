d8a907221addc70d8d31da1feb43db9e1f3b5a1d
dasaris1 <dasarishiva@gmail.com>
2018-11-16 20:23:46 -0800

    updated ...

--- 03_Lists.py
+++ 03_Lists.py
@@ -22,11 +22,37 @@ def method4():
     l = range(num)
     print l[0]
 
+def list_operations():
+    l = [1, 2, 3, 4, 5]
+    m = [99, 100]
+
+    l.extend(m) #extends the original list
+    print l
+
+    slice = l[5:7] #slicing the list
+    print slice
+
+    k = [0]*5 #repetitive values declaration
+    print k
+
+def dynamic_lists():
+    import sys
+    n = 10
+    data = []
+    for i in range(n):
+        a = len(data)
+        b = sys.getsizeof(data);
+        print 'length:{0:1d}, size in bytes:{1:3d}'.format(a, b)
+        data.append(i)
+    print data
+
 #method1()
 #method2()
 #method3()
 #method4()
-print timeit.timeit("method1()", "from __main__ import method1, num", number=1)
-print timeit.timeit("method2()", "from __main__ import method2, num", number=1)
-print timeit.timeit("method3()", "from __main__ import method3, num", number=1)
-print timeit.timeit("method4()", "from __main__ import method4, num", number=1)
+#list_operations()
+dynamic_lists()
+#print timeit.timeit("method1()", "from __main__ import method1, num", number=5)
+#print timeit.timeit("method2()", "from __main__ import method2, num", number=5)
+#print timeit.timeit("method3()", "from __main__ import method3, num", number=5)
+#print timeit.timeit("method4()", "from __main__ import method4, num", number=5)
