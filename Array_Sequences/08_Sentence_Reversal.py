1e276e7c63fc6b66848235a48d519aabb53c4b97
dasaris1 <dasarishiva@gmail.com>
2018-11-18 14:53:06 -0800

    Sentence_Reversal

---
+++ Array_Sequences/08_Sentence_Reversal.py
@@ -0,0 +1,24 @@
+'''
+
+    Reverse the given sentence.
+        Input: 'This is the best'
+        Output: 'best the is This'
+
+    Note: As part of this exercise you should remove all leading and trailing whitespace.
+
+    1) Method1: use python built in functions split() & reversed()
+    2) Method2: use python split() & a trick of printing left to right.
+    3)
+
+'''
+
+class SentenceReversal:
+
+    def __init__(self, sent):
+        self.sent = sent
+
+    def method01(self):
+
+
+
+
