444ffb4a547ab68a40583cdf6093e91bb2f236d1
dasaris1 <dasarishiva@gmail.com>
2018-11-17 18:02:13 -0800

    Array Pair Sum

--- Array_Sequences/05_Array_Pair_Sum.py
+++ Array_Sequences/05_Array_Pair_Sum.py
@@ -6,3 +6,31 @@ Output:
  (2,2)
 '''
 
+class ArrayPairSum:
+
+    def __init__(self, arr, target):
+        self.arr = arr
+        self.target = target
+
+    def method(self):
+
+        'Edge case check
+        if len(self.arr) < 2:
+            return
+
+        #Sets for trackin
+        seen = set()
+        output = set()
+
+        for num in self.arr:
+            diff = self.target - num
+            if diff not in seen:
+                seen.add(num)
+            else:
+                output.add((min(diff,num),max(diff, num)))
+        print output
+        print seen
+        print '\n'.join(map(str,list(output)))
+
+obj1 = ArrayPairSum([1,3,2,2,5,-1],4)
+obj1.method()
