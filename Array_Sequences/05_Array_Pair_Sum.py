65e1d6b51f25afe9a2ed24de9ba38bc71ea7a0be
dasaris1 <dasarishiva@gmail.com>
2018-11-17 18:10:27 -0800

    Array Pair Sum

--- Array_Sequences/05_Array_Pair_Sum.py
+++ Array_Sequences/05_Array_Pair_Sum.py
@@ -1,9 +1,9 @@
 '''
-Given an integer array, output all the unique pairs that sum up to a specific value k.
-Input: pair_sum([1,3,2,2],4)
-Output:
- (1,3)
- (2,2)
+    Given an integer array, output all the unique pairs that sum up to a specific value k.
+    Input: pair_sum([1,3,2,2],4)
+    Output:
+     (1,3)
+     (2,2)
 '''
 
 class ArrayPairSum:
